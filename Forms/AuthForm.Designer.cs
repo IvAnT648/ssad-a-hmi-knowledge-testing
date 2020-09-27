8def7326ee7bb3e83e4ab67f26f315461c8d5c7e
IvAnT <ant.ivan99@mail.ru>
2020-09-27 23:33:50 +0300

    Добавьте файлы проекта.

---
+++ Forms/AuthForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Knowledge_Testing.Forms
+{
+	partial class AuthForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.labelLogin = new System.Windows.Forms.Label();
+			this.labelPassword = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.buttonEnter = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// labelLogin
+			// 
+			this.labelLogin.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.labelLogin.AutoSize = true;
+			this.labelLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelLogin.Location = new System.Drawing.Point(51, 52);
+			this.labelLogin.Name = "labelLogin";
+			this.labelLogin.Size = new System.Drawing.Size(68, 25);
+			this.labelLogin.TabIndex = 0;
+			this.labelLogin.Text = "Логин";
+			// 
+			// labelPassword
+			// 
+			this.labelPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.labelPassword.AutoSize = true;
+			this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelPassword.Location = new System.Drawing.Point(51, 105);
+			this.labelPassword.Name = "labelPassword";
+			this.labelPassword.Size = new System.Drawing.Size(80, 25);
+			this.labelPassword.TabIndex = 1;
+			this.labelPassword.Text = "Пароль";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(152, 53);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(237, 27);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(152, 106);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(237, 27);
+			this.textBox2.TabIndex = 3;
+			// 
+			// buttonEnter
+			// 
+			this.buttonEnter.Location = new System.Drawing.Point(152, 166);
+			this.buttonEnter.Name = "buttonEnter";
+			this.buttonEnter.Size = new System.Drawing.Size(188, 37);
+			this.buttonEnter.TabIndex = 0;
+			this.buttonEnter.Text = "Войти";
+			this.buttonEnter.UseVisualStyleBackColor = true;
+			// 
+			// AuthForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(454, 245);
+			this.Controls.Add(this.buttonEnter);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.labelPassword);
+			this.Controls.Add(this.labelLogin);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AuthForm";
+			this.Text = "Knowledge Testing - Вход в систему";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelLogin;
+		private System.Windows.Forms.Label labelPassword;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button buttonEnter;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Knowledge_Testing.Forms;
+
+namespace Knowledge_Testing
+{
+	static class Program
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
